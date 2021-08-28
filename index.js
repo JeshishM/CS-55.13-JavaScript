41c12960575452eba1549d64a846341de92efa1f
Jeshish Maharjan <89277664+JeshishM@users.noreply.github.com>
2021-08-28 01:23:39 +0000

    Changed code

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-'use strict';
-let p = 25;
-let t = 7;
-let r = 10;
-let simpleinterest = p * t * r / 100;
-console.log(" The Simple Interest = " + simpleinterest);
+// Node Server
+
+const http = require('http');
+const server = http.createServer(
+
+	function(req, res){
+		console.log(req.url );
+
+		let mydata;
+    if ( req.url === "/hello" ) {
+      mydata = "Hey there. - Welcome to my Server...";
+    } else {
+      mydata = "I don't know you!";
+    }
+    mydata = mydata + " " + "Thank You!";
+
+		res.writeHead( 200, { "Content-Type": "text/plain" } );
+		res.end(mydata);
+	}
+);
+server.listen(8080, "0.0.0.0");
+console.log("server has started");
